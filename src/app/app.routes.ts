ef3f1ba558b4107bb3926ca447221ea270ad174e
Juan Douglas <juandouglas2004@gmail.com>
2024-02-28 12:23:18 -0300

    Create Guards Authentication

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AuthenticationComponent } from './authentication/authentication.component';
+import { AccountComponent } from './account/account.component';
+import { AuthGuard } from './auth.guard';
+import { NgModule, inject } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'authentication', component: AuthenticationComponent },
+    { path: 'account', component: AccountComponent, canActivate: [AuthGuard] }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
